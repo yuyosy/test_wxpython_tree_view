80dbea0b499f03d2d2ca5c0ab2561dec166f4fe0
yuyosy <y-ysss@outlook.com>
2022-09-15 01:46:09 +0900

    Update(src/main.py): remove comments

--- src/main.py
+++ src/main.py
@@ -14,14 +14,6 @@ class MainFrame(wx.Frame):
         self.__set_layout()
 
     def __set_layout(self):
-        # notebook = wx.Notebook(self, wx.ID_ANY)
-        # tree_panel = TreePanel(notebook)
-        # notebook.InsertPage(0, tree_panel, 'Tree')
-        # stdout_panel = StdoutPanel(self)
-        # root_layout = wx.BoxSizer(wx.VERTICAL)
-        # root_layout.Add(notebook, proportion=1, flag=wx.ALL | wx.EXPAND, border=5)
-        # root_layout.Add(stdout_panel, proportion=0, flag=wx.ALL | wx.EXPAND)
-        # self.SetSizer(root_layout)
         root_layout = wx.SplitterWindow(self)
         notebook = wx.Notebook(root_layout, wx.ID_ANY)
         tree_panel = TreePanel(notebook)
